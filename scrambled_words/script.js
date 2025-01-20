0e248e9ffa7ccb73f75689a2a9b01e38e9d06b41
kalkidan-teklay <kalkidan.teklehaimanot@aait.edu.et>
2025-01-21 00:04:23 +0300

    frontend

---
+++ scrambled_words/script.js
@@ -0,0 +1,67 @@
+let word = ''; // Global word variable
+
+function createInputBoxes(word) {
+    const input_container = document.getElementById("input_boxes");
+    input_container.innerHTML = ''; 
+    for (let char of word) {
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.maxLength = 1;
+        input.className = 'input-box';
+        input_container.appendChild(input);
+    }
+}
+
+async function fetchRandomWord() {
+    try {
+        const response = await fetch('https://random-word-api.herokuapp.com/word?number=1');
+        const data = await response.json();
+        
+        if (data.length > 0) {
+            word = data[0]; // Assign fetched word to global variable
+            console.log(word);
+            const shuffledWord = shuffleString(word);
+            const paragraph = document.getElementById("generated_text");
+            
+            createInputBoxes(word);
+
+            const wrappedText = shuffledWord.split('').map(letter => {
+                return `<span class="letters">${letter}</span>`;
+            }).join('');
+
+            paragraph.innerHTML = wrappedText; 
+        }
+    } catch (error) {
+        console.error('Error fetching random word:', error);
+    }
+}
+
+function shuffleString(str) {
+    const arr = str.split(''); 
+    for (let i = arr.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1)); 
+        [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr.join(''); 
+}
+
+function checkAnswer() {
+    const inputBoxes = document.querySelectorAll('.input-box');
+    let userGuess = '';
+
+    inputBoxes.forEach(input => {
+        userGuess += input.value.trim(); 
+    });
+
+    const resultMessage = document.getElementById("result_message");
+    console.log("word: ", word);
+    console.log("userGuess: ", userGuess);
+    if (userGuess.toLowerCase() === word.toLowerCase()) {
+        resultMessage.textContent = "Correct! You guessed the word!";
+    } else {
+        resultMessage.textContent = "Incorrect! Try again.";
+    }
+}
+
+document.getElementById("submit_button").addEventListener("click", checkAnswer);
+window.onload = fetchRandomWord;
